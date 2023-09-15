035a6556b474fba5467085718b69fd32a63f4b58
MarinaZhdanovich <marina.zhdanovich@gmail.com>
2023-09-15 14:27:43 +0300

    Added homework 9

---
+++ homework_9/tasks_40_42.py
@@ -0,0 +1,26 @@
+"""
+1. Работать с файлом california_housing_train.csv, который находится в папке sample_data.
+Определить среднюю стоимость дома, где кол-во людей от 0 до 500 (population).
+
+
+2. Узнать какая максимальная households в зоне минимального значения population.
+"""
+
+import pandas as pd
+
+data = pd.read_csv('california_housing_train.csv')
+
+# 1
+# median_house_value - это средняя стоимость дома
+
+res_1 = data[(data['population'] >= 0) & (data['population'] <= 500)]['median_house_value']
+print(res_1)
+
+# или res_1 = data[data['population'] <= 500]['median_house_value']  #количество людей не может быть отрицательным
+# print(res_1)
+print()
+
+# 2
+res_2 = data[data['population'] == data['population'].min()][
+     'households'].max()
+print(res_2)
